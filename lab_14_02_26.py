d87dec0ddbf1c9b7b0f4e26e4e2b8c4b5bc75adc
Michael Polunin <poluninmisa140@gmail.com>
2026-02-12 00:08:50 +0600

    Изменил проверки на функциях AddYearPublication и AddCountBook

--- lab_14_02_26.py
+++ lab_14_02_26.py
@@ -13,6 +13,8 @@
 # Необходимо для каждой книги выдавать количество оставшихся в библиотеке на текущий момент и количество книг несданных в библиотеку (будет выдаваться "Автор" "Название книги" "Сколько ениг не сдано")
 
 # Будет меню, описание класса, в классе будет инициализатор (конструктор), в классе будут методы по обработке записей. Всё остальное на свое усмотрение
+ListObjects = []
+
 class Library:
 
     def __init__(self):
@@ -20,30 +22,61 @@ class Library:
         self.Author = None
         self.YearPublication = None
         self.Reminder = None
+        self.ListObjects = ListObjects
 
     def AddBook(self): 
         self.Book = str(input("Введите название книги:"))
         print(f"Название {self.Book} успешно сохранено")
+
         self.Author = str(input("Введите автора:"))
         print(f"Автор {self.Author} книги {self.Book} успешно сохранён")
-        AddYearPublication = int(input("Введите год издания книги:"))
-        if 1 <= AddYearPublication <= 2026:
-            self.YearPublication = AddYearPublication
-            print(f"Год издания {self.YearPublication} успешно сохранён")
-        else:
-            print(f"Год издания {AddYearPublication} не верен, в нашей библиотеке нет книг выпущенных до нашей эры или произведенний полученных из будущего")
-            AddYearPublication = int(input("Введите новое значение в рамках от 1 до 2026г:"))
-            print(f"Год издания {self.YearPublication} успешно сохранён")
-        
-    def GetAllBooks(self):
-        print(self.Book , self.Author, self.YearPublication, self.Reminder , sep = "/n")
 
-obj = Library()
-obj.AddBook()
-obj2 = Library()
-obj2.AddBook()
-obj.GetAllBooks()
-obj2.GetAllBooks()
+        while True:
+            try:
+                AddYearPublication = int(input("Введите год издания книги:"))
+                if 1 <= AddYearPublication <= 2026:
+                    self.YearPublication = AddYearPublication
+                    print(f"Год издания {self.YearPublication} успешно сохранён")
+                    break
+                else:
+                    print("Год должен быть от 1 до 2026!")
+            except ValueError:
+               print("Введите корректное число")
+
+        while True:
+            try:    
+                AddCountBook = int(input("Введите добавляемое кол-во книг:"))
+                if AddCountBook > 0:
+                    self.Reminder = AddCountBook
+                    print(f"Книга {self.Book} - в количесвте {self.Reminder} Год издания - {self.YearPublication} Автор - {self.Author} успешно сохранена")
+                    break
+                else:
+                    print("Число должно быть >0!!!")
+
+            except ValueError:
+                print("Вводите корректное кол-во книг, значение > 0")
+
+    def FindBookYearPublication(self):
+        FindedBookYearPublication = int(input("Введите искомый год публикации:"))
+        for obj in ListObjects:
+            print(obj)
+
+def InitNewObject():
+        x = input("Введите имя нового объекта:")
+        x = Library()
+        x.AddBook()
+        ListObjects.append(x)
+        x.FindBookYearPublication()
+InitNewObject()
+
+
+
+
+    
+
+
+
+
 
 
 
